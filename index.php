6835bfeba04304f91eaceb7d29515d03884987a7
michalLenczowski <michalek118@gmail.com>
2017-02-02 15:48:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,13 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>SuperCRM</title>
-
+        <title>Forms Generator</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300,600,400&subset=latin-ext' rel='stylesheet' type='text/css'>
+        
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link href='http://fonts.googleapis.com/css?family=Raleway:300,200,100' rel='stylesheet' type='text/css'>
 
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
@@ -44,20 +48,20 @@ $(document).ready(function(){
                     </div>
                     <nav class="sidebar-navigation">
                         <ul>
-                            <li  class="active">
-                                <a  href="/#/formularz">
-                                    <i class="fa fa-users"></i>
+                            <li ng-class="{active: routetest.isActive('/formularz')}">
+                                <a  href="index.php#/formularz">
+                                    <i class="fa fa-users"></i>
                                     <span>Generowanie</span>
                                 </a>
                             </li>
-                            <li >
-                                <a href="/#/podglad">
+                            <li ng-class="{active: routetest.isActive('/podglad')}">
+                                <a href="index.php#/podglad">
                                     <i class="fa fa-database"></i>
                                     <span>Podgląd formularza</span>
                                 </a>
                             </li>
-                            <li >
-                                <a href="/#/kod">
+                            <li ng-class="{active: routetest.isActive('/kod')}">
+                                <a href="index.php#/kod">
                                     <i class="fa fa-cogs"></i>
                                     <span>Wygenerowany kod</span>
                                 </a>
